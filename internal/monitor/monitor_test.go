7b506a988a0dba31ac373525e51d4236e2d8caa0
ybelMekk <youssef.bel.mekki@nav.no>
2023-05-15 12:03:12 +0200

    fix(monitor): clean up tests

--- internal/monitor/monitor_test.go
+++ internal/monitor/monitor_test.go
@@ -7,25 +7,22 @@ import (
 	"os"
 	"testing"
 
+	"picante/internal/test"
+
 	"github.com/nais/dependencytrack/pkg/client"
 
 	"github.com/in-toto/in-toto-golang/in_toto"
 	"github.com/nais/dependencytrack/pkg/httpclient"
-	"github.com/stretchr/testify/mock"
-	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"picante/internal/attestation"
-	"picante/internal/pod"
-
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"picante/internal/attestation"
 )
 
 func TestConfig_OnAdd(t *testing.T) {
 	c := NewMockClient(t)
 	v := attestation.NewMockVerifier(t)
 	m := NewMonitor(context.Background(), c, v)
-	p := createPod("team1", "pod1", nil, "nginx:latest")
+	p := test.CreatePod("team1", "pod1", nil, "nginx:latest")
 
 	var statement in_toto.CycloneDXStatement
 	file, err := os.ReadFile("testdata/sbom.json")
@@ -90,7 +87,7 @@ func TestConfig_OnDelete(t *testing.T) {
 	c := NewMockClient(t)
 	v := attestation.NewMockVerifier(t)
 	m := NewMonitor(context.Background(), c, v)
-	p := createPod("team1", "pod1", nil, "nginx:latest")
+	p := test.CreatePod("team1", "pod1", nil, "nginx:latest")
 
 	t.Run("should delete project", func(t *testing.T) {
 		c.On("DeleteProjects", mock.Anything, "pod1:nginx").Return(nil)
@@ -106,8 +103,8 @@ func TestConfig_OnUpdate(t *testing.T) {
 	c := NewMockClient(t)
 	v := attestation.NewMockVerifier(t)
 	m := NewMonitor(context.Background(), c, v)
-	p := createPod("team1", "pod1", nil, "nginx:latest")
-	pLatest := createPod("team1", "pod1", nil, "nginx:laterthenlatest")
+	p := test.CreatePod("team1", "pod1", nil, "nginx:latest")
+	pLatest := test.CreatePod("team1", "pod1", nil, "nginx:laterthenlatest")
 
 	t.Run("should ignore old and new pod with equal container image(s) ", func(t *testing.T) {
 		m.OnUpdate(p, p)
@@ -139,44 +136,6 @@ func TestConfig_OnUpdate(t *testing.T) {
 	t.Run("should ignore nil pod object", func(t *testing.T) {
 		m.OnUpdate(p, nil)
 	})
-}
-
-func createPod(namespace, name string, labels map[string]string, images ...string) *v1.Pod {
-	c := make([]v1.Container, 0)
-	for _, image := range images {
-		c = append(c, v1.Container{
-			Image: image,
-		})
-	}
-	l := merge(map[string]string{
-		pod.SalsaKeylessProviderLabelKey:  "cosign",
-		pod.SalsaKeyRefLabelKey:           "testdata/cosign.key",
-		pod.SalsaPredicateLabelKey:        "cyclonedx",
-		pod.TeamLabelKey:                  namespace,
-		pod.IgnoreTransparencyLogLabelKey: "true",
-		pod.AppK8sIoNameLabelKey:          name,
-	}, labels)
-	return &v1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
-			Namespace: namespace,
-			Labels:    l,
-		},
-		Spec: v1.PodSpec{
-			Containers: c,
-		},
-	}
-}
-
-func merge(map1, map2 map[string]string) map[string]string {
-	mergedMap := make(map[string]string)
-	for key, value := range map1 {
-		mergedMap[key] = value
-	}
-	for key, value := range map2 {
-		mergedMap[key] = value
-	}
-	return mergedMap
 }
 
 func TestProjectAndVersion(t *testing.T) {
--- internal/pod/pod_test.go
+++ internal/pod/pod_test.go
@@ -1,9 +1,9 @@
 package pod
 
 import (
-	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"testing"
+
+	"picante/internal/test"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -48,7 +48,7 @@ func TestGetInfo(t *testing.T) {
 			l[SalsaPredicateLabelKey] = tt.predicateType
 			l[SalsaKeyRefLabelKey] = tt.keyRef
 			l[IgnoreTransparencyLogLabelKey] = tt.tlog
-			p := createPod("team1", "pod1", l, "nginx:latest")
+			p := test.CreatePod("team1", "pod1", l, "nginx:latest")
 
 			inf := GetInfo(p)
 			assert.Equal(t, tt.wantPredicateType, inf.GetPredicateType(), "predicate type should match")
@@ -59,44 +59,7 @@ func TestGetInfo(t *testing.T) {
 			assert.Equal(t, "pod1", inf.Name, "name should match")
 			assert.Equal(t, "team1", inf.Namespace, "namespace should match")
 			assert.Equal(t, "nginx:latest", inf.ContainerImages[0].Image, "image should match")
+			assert.Equal(t, "pod1", inf.ContainerImages[0].Name, "name should match")
 		})
 	}
-}
-
-func createPod(namespace, name string, labels map[string]string, images ...string) *v1.Pod {
-	c := make([]v1.Container, 0)
-	for _, image := range images {
-		c = append(c, v1.Container{
-			Image: image,
-		})
-	}
-	l := merge(map[string]string{
-		SalsaKeylessProviderLabelKey:  "cosign",
-		SalsaKeyRefLabelKey:           "testdata/cosign.key",
-		SalsaPredicateLabelKey:        "cyclonedx",
-		TeamLabelKey:                  namespace,
-		IgnoreTransparencyLogLabelKey: "true",
-		AppK8sIoNameLabelKey:          name,
-	}, labels)
-	return &v1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
-			Namespace: namespace,
-			Labels:    l,
-		},
-		Spec: v1.PodSpec{
-			Containers: c,
-		},
-	}
-}
-
-func merge(map1, map2 map[string]string) map[string]string {
-	mergedMap := make(map[string]string)
-	for key, value := range map1 {
-		mergedMap[key] = value
-	}
-	for key, value := range map2 {
-		mergedMap[key] = value
-	}
-	return mergedMap
 }

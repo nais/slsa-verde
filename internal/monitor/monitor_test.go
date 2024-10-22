dc3af61c501d6c67b219430e05377d76eeb12768
Tommy Trøen <tommy.troen@nav.no>
2024-10-22 14:51:53 +0200

    fix: add expected calls to tests
    Co-authored-by: ybelmekk <youssef.bel.mekki@nav.no>
    Co-authored-by: sindrerh2 <sindre.rodseth.hansen@nav.no>

--- internal/monitor/monitor_test.go
+++ internal/monitor/monitor_test.go
@@ -84,6 +84,8 @@ func TestConfigOnAdd(t *testing.T) {
 			Uuid: "uuid1",
 		}, nil)
 		c.On("UploadProject", mock.Anything, "test/nginx", "latest", "uuid1", false, mock.Anything).Return(nil, nil)
+		c.On("TriggerAnalysis", mock.Anything, "uuid1").Return(nil)
+		c.On("GetProject", mock.Anything, "test/nginx", "latest").Return(&client.Project{Uuid: "uuid1"}, nil)
 
 		m.OnAdd(deployment)
 	})
@@ -194,6 +196,9 @@ func TestConfigOnAddSeveralProjectsFromContainers(t *testing.T) {
 		c.On("GetProjectsByTag", mock.Anything, url.QueryEscape(workload.getTag(cluster))).Return([]*client.Project{}, nil)
 		c.On("CreateProject", mock.Anything, "test/nginx", "latest", "test", tags).Return(&client.Project{Uuid: "uuid1"}, nil)
 		c.On("UploadProject", mock.Anything, "test/nginx", "latest", "uuid1", false, mock.Anything).Return(nil, nil)
+		c.On("TriggerAnalysis", mock.Anything, "uuid1").Return(nil)
+		c.On("GetProject", mock.Anything, "test/nginx", "latest").Return(&client.Project{Uuid: "uuid1"}, nil)
+		c.On("TriggerAnalysis", mock.Anything, "uuid2").Return(nil)
 		c.On("GetProject", mock.Anything, "test/nginx", "latest2").Return(nil, nil)
 
 		att = &attestation.ImageMetadata{
@@ -211,7 +216,6 @@ func TestConfigOnAddSeveralProjectsFromContainers(t *testing.T) {
 
 		m.OnAdd(deployment)
 	})
-
 	t.Run("should not create project if no metadata is found", func(t *testing.T) {
 		c.On("GetProject", mock.Anything, "test/nginx", "latest").Return(nil, nil)
 		v.On("Verify", mock.Anything, mock.Anything).Return(&attestation.ImageMetadata{}, nil)
@@ -323,7 +327,7 @@ func TestConfigOnAddExistsJobWithNewVersion(t *testing.T) {
 		tags := workload.initWorkloadTags(att, cluster, "test/nginx", "latest2")
 		c.On("CreateProject", mock.Anything, "test/nginx", "latest2", "test", tags).Return(&client.Project{Uuid: "uuid2"}, nil)
 		c.On("UploadProject", mock.Anything, "test/nginx", "latest2", "uuid2", false, mock.Anything).Return(nil, nil)
-
+		c.On("TriggerAnalysis", mock.Anything, "uuid2").Return(nil)
 		m.OnAdd(job)
 	})
 }
@@ -405,6 +409,7 @@ func TestConfigOnAddExists(t *testing.T) {
 		tags := workload.initWorkloadTags(att, cluster, "test/nginx", "latest2")
 		c.On("CreateProject", mock.Anything, "test/nginx", "latest2", "test", tags).Return(&client.Project{Uuid: "uuid2"}, nil)
 		c.On("UploadProject", mock.Anything, "test/nginx", "latest2", "uuid2", false, mock.Anything).Return(nil, nil)
+		c.On("TriggerAnalysis", mock.Anything, "uuid2").Return(nil)
 
 		m.OnAdd(deployment)
 	})
